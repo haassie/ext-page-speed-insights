ece2010394b43ef597c4409df968703ccf3b668a
Richard Haeser <richard@maxserv.com>
2019-10-03 20:24:54 +0200

    [TASK] Initial setup of TYPO3 extension

---
+++ ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Page Speed Insights',
+    'description' => 'Page Speed Insights',
+    'category' => 'fe',
+    'author' => 'Richard Haeser',
+    'author_email' => 'richardhaeser@gmail.com',
+    'author_company' => '',
+    'state' => 'beta',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '8.7.0 - 10.4.99'
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
