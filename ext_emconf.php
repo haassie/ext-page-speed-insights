8beda8d1488267f617cfe1fb19936dc1ebe31c38
Richard Haeser <richard@maxserv.com>
2019-11-04 14:31:14 +0100

    [RELEASE] Release of v1.0.1 of the extension with some additional metadata

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,16 +1,16 @@
 <?php
 $EM_CONF[$_EXTKEY] = [
     'title' => 'PageSpeed Insights',
-    'description' => 'PageSpeed Insights',
+    'description' => 'Check the performance of your pages in TYPO3 with PageSpeed Insights.',
     'category' => 'fe',
     'author' => 'Richard Haeser',
     'author_email' => 'richardhaeser@gmail.com',
     'author_company' => '',
-    'state' => 'beta',
+    'state' => 'stable',
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.0.0',
+    'version' => '1.0.1',
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0 - 9.5.99'
