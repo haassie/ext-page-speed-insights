a87406e3f6ba4fb514bbcc2390a83091cefb4534
Richard Haeser <richard@maxserv.com>
2020-02-10 16:38:21 +0100

    [TASK] Prepare v10 support

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,10 +10,10 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.1.0',
+    'version' => '2.0.0-dev',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0 - 9.5.99'
+            'typo3' => '9.5.0 - 10.4.99'
         ],
         'conflicts' => [],
         'suggests' => [],
