e67cd9ad7ea73c159c809e44dfd780b0e35e96d1
Richard Haeser <richard@maxserv.com>
2020-02-16 10:53:54 +0100

    [TASK] Change to new way of registering dashboard presets

---
+++ Configuration/Backend/DashboardPresets.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'dashboardPreset-lighthouse' => [
+        'title' => 'Lighthouse',
+        'description' => '',
+        'iconIdentifier' => 'dashboard-empty',
+        'defaultWidgets' => [],
+        'showInWizard' => true
+    ],
+];
